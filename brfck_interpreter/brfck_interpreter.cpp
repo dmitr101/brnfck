43fa39624c19f9584fca16d067bf1ba24a7e6ae8
Dmitry <dmitr5.11.1997@gmail.com>
2017-08-08 16:56:57 +0300

    first commit, all basic code, may cantain a lot of bugs

---
+++ brfck_interpreter/brfck_interpreter.cpp
@@ -0,0 +1,169 @@
+// brfck_interpreter.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#define CELL_COUNT 30000
+#define DEFAULT_NESTING 15
+
+struct env
+{
+	char*	arr;
+	size_t	ind;
+
+	const char**	nstck;
+	size_t	nsize;
+	size_t	msize;
+};
+
+env* create_env(size_t size = CELL_COUNT)
+{
+	env* e = (env*)malloc(sizeof(env));
+	e->arr = (char*)malloc(size);
+	e->ind = 0;
+	e->nstck = (const char**)malloc(sizeof(*e->nstck)*DEFAULT_NESTING);
+	e->nsize = 0;
+	e->msize = DEFAULT_NESTING;
+	return e;
+}
+
+void destroy_env(env* e)
+{
+	free(e->arr);
+	free(e->nstck);
+	free(e);
+}
+
+void realloc_nstck(env* e)
+{
+	char** tmp = (char**)malloc(e->msize);
+	memcpy(tmp, e->nstck, e->nsize);
+	free(e->nstck);
+	e->msize += DEFAULT_NESTING;
+	e->nstck = (const char**)malloc(e->msize);
+	memcpy(e->nstck, tmp, e->nsize);
+}
+
+void push_nstck(env* e, const char* ptr)
+{
+	if (e->nsize == e->msize)
+	{
+		realloc_nstck(e);
+	}
+	e->nstck[e->nsize] = ptr;
+	e->nsize++;
+}
+
+void pop_nstck(env* e)
+{
+	e->nsize--;
+}
+
+const char* get_nstck(env* e)
+{
+	return e->nstck[e->nsize - 1];
+}
+
+void process_script(env* e, const char* script)
+{
+	const char* cur = script;
+	while (*cur++)
+	{
+		switch (*cur)
+		{
+		case '+':
+			e->arr[e->ind]++;
+			break;
+		case '-':
+			e->arr[e->ind]--;
+			break;
+		case '>':
+			e->ind++;
+			break;
+		case '<':
+			e->ind--;
+			break;
+		case '.':
+			putchar(e->arr[e->ind]);
+			break;
+		case ',':
+			e->arr[e->ind] = getchar();
+			break;
+		case '[':
+		{
+			if (e->arr[e->ind])
+			{
+				push_nstck(e, cur);
+				break;
+			}
+			int bal = -1;
+			while (*cur++ && !bal)
+				if (*cur == '[')
+					bal--;
+				else if (*cur == ']')
+					bal++;
+			break;
+		}
+		case ']':
+		{
+			if (e->arr[e->ind])
+			{
+				cur = get_nstck(e);
+				break;
+			}
+			pop_nstck(e);
+			break;
+		}
+		}
+	}
+}
+
+void run_interactive(env* e)
+{
+	char line[80];
+	do
+	{
+		printf("brfck)>");
+		fflush(stdout);
+		scanf("%79s", line);
+		if (!strcmp(line, "quit"))
+			break;
+		process_script(e, line);
+	} while (true);
+}
+
+void run_module(env* e, const char* filename)
+{
+	FILE* fptr = fopen(filename, "r");
+	fseek(fptr, 0L, SEEK_END);
+	size_t sz = ftell(fptr);
+	rewind(fptr);
+	char* script = (char*)malloc(sz + 1);
+
+	size_t ind = 0;
+	while (!feof(fptr))
+	{
+		script[ind] = fgetc(fptr);
+		ind++;
+	}
+	script[ind + 1] = '\0';
+	process_script(e, script);
+}
+
+int main(int argc, char* argv[])
+{
+	env* environment = create_env();
+	switch (argc)
+	{
+	case 1:
+		run_interactive(environment);
+		break;
+	case 2:
+		run_module(environment, argv[1]);
+		break;
+	default:
+		printf("Error - invalid number of args \n Valid usage: no args for intaractive mode or filename for module run");
+		return -3;
+	}
+	destroy_env(environment);
+	return 0;
+}
